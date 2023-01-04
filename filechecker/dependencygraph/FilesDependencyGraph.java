1eddb66c9ff0a55bbfdb5515d5f4256639d19101
Ilya <KobyzevIlya@gmail.com>
2023-01-05 00:05:41 +0300

    Changed @override in FilesDependencyGraph

--- filechecker/dependencygraph/FilesDependencyGraph.java
+++ filechecker/dependencygraph/FilesDependencyGraph.java
@@ -14,27 +14,17 @@ public class FilesDependencyGraph extends AbstractGraph<File> {
     }
 
     @Override
-    public Node<File> getNode(File data) {
-        for (var node : nodes) {
-            if (node.getData().equals(data)) {
-                return node;
-            }
-        }
-        return null;
-    }
-
-    @Override
     public void addNode(File file) {
         Node<File> currentNode = getNode(file);
         if (currentNode == null) {
-            nodes.add(new Node<>(file));
+            super.addNode(file);
             currentNode = getNode(file);
         }
 
         for (var requirement : fileHandler.getRequires(file)) {
             Node<File> parent = getNode(requirement);
             if (parent == null) {
-                nodes.add(new Node<>(requirement));
+                super.addNode(requirement);
                 parent = getNode(requirement);
             }
             parent.addDescendant(currentNode);
